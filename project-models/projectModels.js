390205ea6d06bf8dcb513d70e3318341ea35bce2
unknown <tompsherman@gmail.com>
2020-11-06 12:55:40 -0500

    get endpoints up and running

--- project-models/projectModels.js
+++ project-models/projectModels.js
@@ -3,12 +3,35 @@ const db = require('../data/db-config')
 module.exports = {
     //function names
 
-    find,
+    findProjects,
+    findResources,
+    findTasks, // join with project name and description
+
+    createProject,
+    createResource,
+    createTask
 
 }
 
 //funstions 
 
-function find(){
+function findProjects(){
     return db('projects')
+}
+function findResources(){
+    return db('resources')
+}
+function findTasks(){
+    return db('tasks as t')
+    .join('projects as p', 'p.id', 't.project_id')
+    .select('t.task_name', 't.task_notes', 'p.proj_name', 'p.proj_description')
+}
+function createProject(){
+    return db('projects')
+}
+function createResource(){
+    return db('resources')
+}
+function createTask(){
+    return db('tasks')
 }
--- project-models/projectRouter.js
+++ project-models/projectRouter.js
@@ -12,8 +12,33 @@ router.get('/test', (req, res) => {
 // @desc		Get all projects
 // @route		GET /
 router.get('/', (req, res) => {
-
+    Project.findProjects()
+    .then(projects => {
+        res.status(202).json(projects)
+    })
+    .catch(error => res.status(500).json({message: `${error.message}; ${error.stack}`}))
 })
+
+// @desc		Get all resources
+// @route		GET /
+router.get('/resources', (req, res) => {
+    Project.findResources()
+    .then(resources => {
+        res.status(202).json(resources)
+    })
+    .catch(error => res.status(500).json({message: `${error.message}; ${error.stack}`}))
+})
+
+// @desc		Get all resources
+// @route		GET /
+router.get('/tasks', (req, res) => {
+    Project.findTasks()
+    .then(tasks => {
+        res.status(202).json(tasks)
+    })
+    .catch(error => res.status(500).json({message: `${error.message}; ${error.stack}`}))
+})
+
 // @desc		Get a project by id
 // @route		GET /:id
 router.get('/:id', (req, res) => {
